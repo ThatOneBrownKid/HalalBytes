a50cd959226b76ddd71b53d8bc2a8a5d540394e5
xlor1009 <108946864+xlor1009@users.noreply.github.com>
2023-05-16 13:20:47 -0400

    set up devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_042414) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_171509) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -21,6 +21,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_09_042414) do
     t.integer "overall_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
