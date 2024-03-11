350c08be80ce3777550a41aae92a7abbe3d92753
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-03-11 01:40:09 -0400

    Image fix (#6)
    * No login required
    
    * Image Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def process_image_for_carousel(image, width, height)
+        image.variant(
+          resize_to_fill: [width, height, { gravity: 'Center' }]
+          # Note: MiniMagick backend might not directly support 'background' option in the same way
+        )
+      end
+
 end
