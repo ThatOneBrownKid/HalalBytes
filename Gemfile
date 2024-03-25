a3eaccd98771f50119c59cbb4eb76715bacde38c
Azwad Alam <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-03-25 00:29:14 -0400

    Removed Live Development

--- Gemfile
+++ Gemfile
@@ -84,4 +84,3 @@ group :test do
   gem "webdrivers"
 end
 
-gem "hotwire-livereload", "~> 1.2", :group => :development
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,9 +71,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/assets/stylesheets")
-  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/javascript")
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
